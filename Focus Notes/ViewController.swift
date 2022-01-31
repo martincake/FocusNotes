dee1fbd88ae1914d073570f7d0e3a0fcc5462b5c
Shilonosov Artyom <av.shilonosov@gmail.com>
2022-01-31 21:52:18 +0500

    Initial Commit

---
+++ Focus Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Focus Notes
+//
+//  Created by Артём Шилоносов on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
